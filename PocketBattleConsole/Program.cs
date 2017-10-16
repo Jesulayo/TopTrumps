86a5eb4f6bdd63839cf5cdf07ab75528b20b4264
Sergio Russo <sergio@brandfour.com>
2017-10-16 15:59:21 +0100

    casing

--- PocketBattleConsole/Program.cs
+++ PocketBattleConsole/Program.cs
@@ -18,25 +18,25 @@ namespace PocketBattleConsole
             var controller = locator.GetService<IGameController>();
             var cardFactory = locator.GetService<BaseMonsterFactory>();
 
-            //1. Get the deks for both players
+            //1. Get the decks for both players
 
             var humanDeck = new List<IMonsterCard>();
-            var AiDeck = new List<IMonsterCard>();
+            var aiDeck = new List<IMonsterCard>();
 
             for(int i = 0; i <= 4; i++)
             {
                 humanDeck.Add(cardFactory.GetCard());
-                AiDeck.Add(cardFactory.GetCard());
+                aiDeck.Add(cardFactory.GetCard());
             }
 
             //2. Set decks in game controller
             controller.PlayerOneDeck = humanDeck;
-            controller.PlayerTwoDeck = AiDeck;
+            controller.PlayerTwoDeck = aiDeck;
 
             controller.PlayerOne = new Human();
             controller.PlayerTwo = new BasicAI();
 
-            //3. LET THE GAMES BEGIN
+            //3. Start the game
             controller.Begin();
 
         }
